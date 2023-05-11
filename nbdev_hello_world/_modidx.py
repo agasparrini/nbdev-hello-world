aa8195b37ec906c32cc82bb6c9840e75c7f9bf4f
Andrew Gasparrini <agasparrini@empresstx.com>
2023-05-11 12:13:47 -0400

    add `say_hello`

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://agasparrini.github.io',
                 'git_url': 'https://github.com/agasparrini/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.test': { 'nbdev_hello_world.test.foo': ('test.html#foo', 'nbdev_hello_world/test.py'),
+                                        'nbdev_hello_world.test.say_hello': ('test.html#say_hello', 'nbdev_hello_world/test.py')}}}
